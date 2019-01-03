f01dc3bb86848e6ea876507fba0d4da7b5fecd52
HyungJiny <hyungjin.lee1004@gmail.com>
2019-01-03 19:56:54 +0900

    7287

--- baekjoon/11816/problem.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	int input;
-	scanf("%i", &input);
-	printf("%d\n", input);
-	return 0;
-}
---
+++ baekjoon/7287.cpp
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/7287
+#include <iostream>
+#include <iomanip>
+
+int main(){
+	std::cout << "8\n";
+	std::cout << "racco1004\n";
+
+	return 0;
+}
