96303bcde305679424a9943d605d498f09720778
HyungJiny <hyungjin.lee1004@gmail.com>
2019-01-03 20:22:24 +0900

    11718

---
+++ baekjoon/11718.cpp
@@ -0,0 +1,13 @@
+// https://www.acmicpc.net/problem/11718
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(){
+    string input;
+    while(getline(cin, input)){
+        cout << input << endl;
+    }
+
+    return 0;
+}
