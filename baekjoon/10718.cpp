ca3fc170fa67b4b77d07f937c8afd1fb4ed9033c
HyungJiny <hyungjin.lee1004@gmail.com>
2019-01-03 20:12:54 +0900

    10718

---
+++ baekjoon/10718.cpp
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/10718
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "강한친구 대한육군" << endl;
+    cout << "강한친구 대한육군" << endl;
+
+    return 0;
+}
