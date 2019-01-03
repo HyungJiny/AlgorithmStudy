823992bcb4f904b6a9966a852e34e75efbd07810
HyungJiny <hyungjin.lee1004@gmail.com>
2019-01-03 22:44:32 +0900

    c++에서는 cout 보다 printf가 더 빠름

---
+++ baekjoon/2741.cpp
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/2741
+#import <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=n; i++){
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
