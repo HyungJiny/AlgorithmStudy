d85eb01fff7af4d29c5a1d1d004e1da2f599a3ae
HyungJiny <hyungjin.lee1004@gmail.com>
2019-01-03 20:25:43 +0900

    1단계 끝

---
+++ baekjoon/11719.cpp
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/11719
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string input_line;
+    while(getline(cin, input_line)){
+        cout << input_line << endl;
+    }
+    return 0;
+}
