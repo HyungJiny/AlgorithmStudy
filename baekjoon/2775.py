faeca7ff207c8ffd77d07f3dec60eef9b95a98ed
HyungJiny <hyungjin.lee1004@gmail.com>
2019-01-27 14:30:34 +0900

    2775번 리스트로 해결

--- baekjoon/2775.py
+++ baekjoon/2775.py
@@ -1,30 +1,20 @@
 # https://www.acmicpc.net/problem/2775
 import sys
 
-rooms = [[0 for _ in range(15)] for _ in range(15)]
-
-def reset_rooms():
-    rooms = [[0 for _ in range(15)] for _ in range(15)]
-
 def number_of_resident(k, n):
-    if k==0:
-        rooms[k][n] = n
-        return rooms[k][n]
-    if n==1: 
-        rooms[k][n] = 1
-        return rooms[k][n]
-    if rooms[k-1][n] == 0: prev_room = number_of_resident(k-1, n)
-    else: prev_room = rooms[k-1][n]
-    if rooms[k][n-1] == 0: underflow_room = number_of_resident(k, n-1)
-    else: underflow_room = rooms[k][n-1]
-    return prev_room + underflow_room
+    floor = [[0 for _ in range(n)] for _ in range(k+1)]
+    for i in range(k+1):
+        for j in range(n):
+            if i==0: floor[i][j] = j+1
+            elif j==0: floor[i][j] = 1
+            else: floor[i][j] = floor[i-1][j] + floor[i][j-1]
+    return floor[k][n-1]
 
 def main():
     for _ in range(int(sys.stdin.readline())):
         k = int(sys.stdin.readline())
         n = int(sys.stdin.readline())
         print(number_of_resident(k, n))
-        reset_rooms()
 
 if __name__ == "__main__":
     main()
