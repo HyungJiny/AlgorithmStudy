5649b08ddda419983f14b9bede1cd7737f24f0bd
HyungJiny <hyungjin.lee1004@gmail.com>
2019-02-07 21:28:51 +0900

    1181번 아스키코드로 해결

--- baekjoon/1181.py
+++ baekjoon/1181.py
@@ -1,12 +1,18 @@
 # https://www.acmicpc.net/problem/1181
 import sys
 
+def get_ascii(word):
+    result = ''
+    for i in range(len(word)): result += str('%02d'%(ord(word[i])-96))
+    return int(result)
+
 def main():
     words = []
     for _ in range(int(sys.stdin.readline())):
         words.append(sys.stdin.readline().strip())
     words = list(set(words))
-    print(sorted(words, key=lambda word: len(word)))
+    for word in sorted(words, key=lambda word: get_ascii(word)): 
+        print(word)
 
 if __name__ == "__main__":
     main()
