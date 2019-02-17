144395a1c138b8e0e60d0bb0876ba5e98f489c36
HyungJiny <hyungjin.lee1004@gmail.com>
2019-02-17 17:22:33 +0900

    10989번 적은 메모리와 시간 때문에 고생

--- baekjoon/10989.py
+++ baekjoon/10989.py
@@ -1,13 +1,14 @@
 # https://www.acmicpc.net/problem/10989
-# 메모리 초과
+# 주어지는 메모리와 시간이 적으니 최대한 활용할 수 있어야함
 import sys
 
 def main():
-    numbers = []
+    numbers = [0]*10001
     for _ in range(int(sys.stdin.readline())):
-        numbers.append(int(sys.stdin.readline()))
-    for i in sorted(numbers):
-        print(i)
-
+        numbers[int(sys.stdin.readline())] += 1
+    for i in range(len(numbers)):
+        for _ in range(numbers[i]):
+            print(i)
+    
 if __name__ == "__main__":
     main()
