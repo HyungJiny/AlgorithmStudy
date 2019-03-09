87d047ad6ce5ee40463389cd933e20b661501f57
HyungJiny <hyungjin.lee1004@gmail.com>
2019-03-09 17:20:23 +0900

    2581번 작은수부터 곱해서 소수를 제거

--- baekjoon/2581.py
+++ baekjoon/2581.py
@@ -5,13 +5,23 @@ def main():
     M = int(sys.stdin.readline())
     N = int(sys.stdin.readline())
     numbers = [i for i in range(2, N+1)]
-    for number in numbers:
-        mul = 1
+    index = 0
+    # 소수 구하기
+    while index < len(numbers):
+        mul = 2
+        number = numbers[index]
         while number*mul <= N:
-            if number*mul in numbers: continue
-            numbers.remove(number*mul)
             mul += 1
-    print(numbers)
+            if not number*(mul-1) in numbers: continue
+            else: numbers.remove(number*(mul-1))
+        index += 1
+    numbers = [number for number in numbers if number >= M]
+    
+    # 결과 출력
+    if len(numbers) == 0: print(-1)
+    else:
+        print(sum(numbers))
+        print(numbers[0])
 
 if __name__ == "__main__":
     main()
