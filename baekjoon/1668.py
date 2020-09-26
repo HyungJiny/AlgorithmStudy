59651f475f50294c73a4f415fca421d836138745
HyungJiny <hyungjin.lee1004@gmail.com>
2020-09-26 09:48:52 +0000

    1668

--- baekjoon/1668.py
+++ baekjoon/1668.py
@@ -1,3 +1,21 @@
 # https://www.acmicpc.net/problem/1668
 import sys
 
+n = int(sys.stdin.readline())
+trophies = [int(line) for line in sys.stdin.readlines()]
+
+left_count = 1
+max_trophy = trophies[0]
+for i in range(n):
+    if max_trophy < trophies[i]:
+        left_count += 1
+        max_trophy = trophies[i]
+
+right_count = 1
+max_trophy = trophies[-1]
+for i in range(n-1, -1, -1):
+    if max_trophy < trophies[i]:
+        right_count += 1
+        max_trophy = trophies[i]
+
+print('{}\n{}'.format(left_count, right_count))
